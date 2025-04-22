f10f07db157449770f82072b8adc0915ab29c699
Redream <mr.golyadkina@mail.ru>
2025-04-22 13:44:33 +0300

    feat: add attack cooldown

--- Assets/_Project/Logic/Scripts/Weapon/Bow.cs
+++ Assets/_Project/Logic/Scripts/Weapon/Bow.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Bow : MonoBehaviour
@@ -5,15 +6,33 @@ public class Bow : MonoBehaviour
     [SerializeField] private GameObject _arrowPrefab;
     [SerializeField] private Transform _arrowSpawnPoint;
 
+    [SerializeField] private float _attackSpeed = 1f;
+
+    private bool _canAttack = true;
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Mouse0))
             Attack();
     }
 
     private void Attack()
     {
-        Instantiate(_arrowPrefab, _arrowSpawnPoint.position, transform.parent.rotation);
+        if (_canAttack)
+        {
+            Instantiate(_arrowPrefab, _arrowSpawnPoint.position, transform.parent.rotation);
+            _canAttack = false;
+            StartCoroutine(CooldownRoutine());
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    private IEnumerator CooldownRoutine()
+    {
+        yield return new WaitForSeconds(_attackSpeed);
+        _canAttack = true;
     }
 }
--- Assets/_Project/Logic/Scripts/Weapon/Projectile.cs
+++ Assets/_Project/Logic/Scripts/Weapon/Projectile.cs
@@ -14,18 +14,6 @@ public class Projectile : MonoBehaviour
         transform.Translate(Vector3.right * Time.deltaTime * _projectileSpeed);
     }
 
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    if (collision.gameObject.CompareTag("Player"))
-    //    {
-    //        return;
-    //    }
-    //    else
-    //    {
-    //        Destroy(gameObject);
-    //    }
-    //}
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
